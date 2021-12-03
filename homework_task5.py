b1f4012089407869ac14145c8b9e6513af282085
AlexRad <a_radyukin@mail.ru>
2021-12-03 20:26:55 +0300

    Add binary search in big homework task

--- homework_task5.py
+++ homework_task5.py
@@ -2,16 +2,27 @@
 
 N, M = map(int, input().split())  # N - number of accounts, M - number of employees
 Cn = []  # money on each account
-num = 0  # counter
 for i in range(N):
     Cn.append(int(input()))
-min_in_mas = min(Cn)
+my_max = sum(Cn)
+my_min = 0
 while True:
-    num += 1
+    num = (my_max + my_min) // 2
+    if num == 0:
+        break
     temp_sum = 0
     for i in range(N):
         temp_sum += Cn[i] // num
-    if temp_sum < M:
+    if temp_sum == M:
+        while temp_sum == M:
+            num += 1
+            temp_sum = 0
+            for i in range(N):
+                temp_sum += Cn[i] // num
         num -= 1
         break
+    elif temp_sum < M:
+        my_max = num
+    else:
+        my_min = num
 print(num)
